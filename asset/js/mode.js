43381f82fb28b6fa3bbcf3696786555d2b458c99
Divay_Kansal <97830371+Crusher95574@users.noreply.github.com>
2023-06-05 01:08:00 +0530

    Update mode.js

--- asset/js/mode.js
+++ asset/js/mode.js
@@ -3,10 +3,10 @@ var rasha = document.getElementById("hero-img");
 icon.onclick = function () {
     document.body.classList.toggle("dark-theme");
     if (document.body.classList.contains("dark-theme")) {
-        icon.src = "../asset/image/sunimage.png";
-        rasha.src = "../asset/image/rashahacks.jpg";
+        icon.src = "./asset/image/sunimage.png";
+        rasha.src = "./asset/image/rashahacks.jpg";
     } else {
-        icon.src = "../asset/image/moon.png";
-        rasha.src = "../asset/image/rashahackslight.jpg";
+        icon.src = "./asset/image/moon.png";
+        rasha.src = "./asset/image/rashahackslight.jpg";
     }
 }

3171e4d93457f8c71ffb3f143ccdd6b706445cad
Divay_Kansal <97830371+Crusher95574@users.noreply.github.com>
2023-06-05 00:59:35 +0530

    Update mode.js

--- asset/js/mode.js
+++ asset/js/mode.js
@@ -3,10 +3,10 @@ var rasha = document.getElementById("hero-img");
 icon.onclick = function () {
     document.body.classList.toggle("dark-theme");
     if (document.body.classList.contains("dark-theme")) {
-        icon.src = "../image/sunimage.png";
+        icon.src = "../asset/image/sunimage.png";
         rasha.src = "../asset/image/rashahacks.jpg";
     } else {
-        icon.src = "../image/moon.png";
-        rasha.src = "../image/rashahackslight.jpg";
+        icon.src = "../asset/image/moon.png";
+        rasha.src = "../asset/image/rashahackslight.jpg";
     }
 }
